fba57388b512ffcada74b87b9222be4ca2a72542
TejasAI31 <Tejeshwarsingh1205@gmail.com>
2024-03-10 02:21:16 +0530

    Added some basic text

--- P28 CS Project/MainUI.cpp
+++ P28 CS Project/MainUI.cpp
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+//shows the main hub
+void mainscreen()
+{
+	int n;
+	printf("\033[0;34m");
+	printf("					----------------------\n");
+	printf("					|Welcome to IIT Patna|\n");
+	printf("					----------------------\n");
+	printf("\033[0;32m");
+	printf("\n\nWhat do you wish to do?\n");
+	printf("-----------------------\n");
+	scanf_s("%d", &n);
+	system("cls");
+}
+
+
+
 int main(void)
 {
-	printf("Hello World");
+	mainscreen();
 }
